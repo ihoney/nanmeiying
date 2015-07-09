762d7ded5eaca2bc03eb588f2cf6e2b1f4bb9d5e
liyuexin <1290821453@qq.com>
2015-07-10 00:46:23 +0800

    添加session监听器，修改客户登陆接口

--- web/db/nanmeiying.sql
+++ web/db/nanmeiying.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50527
 File Encoding         : 65001
 
-Date: 2015-07-09 01:53:35
+Date: 2015-07-10 00:46:04
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -27,12 +27,12 @@ CREATE TABLE `customer` (
   `validTime` date DEFAULT '2015-12-12' COMMENT '有效日期',
   PRIMARY KEY (`id`),
   UNIQUE KEY `uk_c_account` (`account`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ----------------------------
 -- Records of customer
 -- ----------------------------
-INSERT INTO `customer` VALUES ('26', '13333333333', 'rootadmin', '0', '2017-04-03');
+INSERT INTO `customer` VALUES ('26', '13333333333', 'rootadmin', '0', '2015-07-10');
 INSERT INTO `customer` VALUES ('27', '13511111111', 'adminroot', '0', '2015-12-12');
 INSERT INTO `customer` VALUES ('28', '13336746733', 'adminroot', '0', '2015-12-12');
 INSERT INTO `customer` VALUES ('31', '13336746833', 'adminroot', '0', '2015-12-12');
@@ -60,5 +60,5 @@ CREATE TABLE `manager` (
 -- ----------------------------
 -- Records of manager
 -- ----------------------------
-INSERT INTO `manager` VALUES ('1', 'nanmeiying', 'nanmeiying', '0', null);
-INSERT INTO `manager` VALUES ('2', 'admin', 'admin', '0', 'F43FD6C1026C980605ABE0CCEDA2827A');
+INSERT INTO `manager` VALUES ('1', 'nanmeiying', 'nanmeiying', '0', '07DAFC8F7C37CEA234384612F3306DBD');
+INSERT INTO `manager` VALUES ('2', 'admin', 'admin', '0', '848134902017A02635C3F0908BC25C36');
